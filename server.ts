cff60c57be9497bce029693846461182282a246e
jnnzz <larocojanlorenz@gmail.com>
2026-04-21 02:52:13 +0800

    feat: done activity

--- server.ts
+++ server.ts
@@ -1,26 +1,43 @@
 // @ts-nocheck
 import express from "express";
+import bodyParser from "body-parser";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import errorHandler from "./_middleware/error-handler";
+import accountsControler from "./accounts/accounts.controller";
+import swaggerDocs from './_helpers/swagger';
 import path from "path";
 import swaggerUi from "swagger-ui-express";
 import YAML from "yamljs";
 
 const app = express();
-const port = Number(process.env.PORT) || 4000;
-const swaggerDocument = YAML.load(path.join(__dirname, "swagger.yaml"));
+// const port = Number(process.env.PORT) || 4000;
+// const swaggerDocument = YAML.load(path.join(__dirname, "swagger.yaml"));
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 app.use(cookieParser());
-app.use(cors({ origin: true, credentials: true }));
 
-app.get("/", (_req, res) => {
-  res.json({ message: "API is running" });
-});
+app.use(cors({origin: (origin, callback) => callback(null, true), credentials: true}));
 
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use('/accounts', accountsControler);
 
+app.use('api-docs', swaggerDocs);
+
+app.use(errorHandler);
+
+const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : 4000;
 app.listen(port, () => {
   console.log(`Server listening on http://localhost:${port}`);
 });
+
+
+// app.get("/", (_req, res) => {
+//   res.json({ message: "API is running" });
+// });
+
+// app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+// app.listen(port, () => {
+//   console.log(`Server listening on http://localhost:${port}`);
+// });
