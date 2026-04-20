ff3bf9afb5cd61c4d6f078677d19169962cc7679
jan lorenz <larocojanlorenz@gmail.com>
2026-04-21 01:26:29 +0800

    commit

--- _helpers/swagger.ts
+++ _helpers/swagger.ts
@@ -1,9 +1,9 @@
 import express from 'express';
 const router = express.Router();
-import swagger from 'swagger-ui-express';
+import swaggerUi from 'swagger-ui-express';
 import YAML from 'yamljs';
 const swaggerDocument = YAML.load('./swagger.yaml');
 
-router.use('/'. swagger.serve, swaggerUI.setup(swaggerDocument));
+router.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 export default router;
--- server.ts
+++ server.ts
@@ -2,9 +2,13 @@
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import path from "path";
+import swaggerUi from "swagger-ui-express";
+import YAML from "yamljs";
 
 const app = express();
 const port = Number(process.env.PORT) || 4000;
+const swaggerDocument = YAML.load(path.join(__dirname, "swagger.yaml"));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -14,6 +18,8 @@ app.use(cors({ origin: true, credentials: true }));
 app.get("/", (_req, res) => {
   res.json({ message: "API is running" });
 });
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.listen(port, () => {
   console.log(`Server listening on http://localhost:${port}`);
