56cc21c917074caefafc6d6730d9498744902434
jan lorenz <larocojanlorenz@gmail.com>
2026-05-17 13:28:48 +0800

    fix: production build with tsc for Render deployment

--- _helpers/swagger.ts
+++ _helpers/swagger.ts
@@ -2,7 +2,8 @@ import express from 'express';
 const router = express.Router();
 import swaggerUi from 'swagger-ui-express';
 import YAML from 'yamljs';
-const swaggerDocument = YAML.load('./swagger.yaml');
+import path from 'path';
+const swaggerDocument = YAML.load(path.join(__dirname, '..', 'swagger.yaml'));
 
 router.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
